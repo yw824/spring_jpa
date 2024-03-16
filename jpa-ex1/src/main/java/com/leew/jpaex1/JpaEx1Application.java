09774c852e00e66459e7ed9bcd120bb6d03f4b3e
def47619 <def47619@gmail.com>
2024-03-16 13:32:00 +0900

    <docs> : first Commit

---
+++ jpa-ex1/src/main/java/com/leew/jpaex1/JpaEx1Application.java
@@ -0,0 +1,13 @@
+package com.leew.jpaex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEx1Application.class, args);
+	}
+
+}
---
+++ jpa-ex1/src/test/java/com/leew/jpaex1/JpaEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leew.jpaex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
